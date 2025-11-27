711d62e30296d77e462d85b1e61cbaf48584a2d8
anand7592 <148118898+anand7592@users.noreply.github.com>
2025-11-27 10:21:00 +0530

    userRoute created

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,30 @@
 const express = require("express");
-const { getUsers, createUser } = require("../controller/userController");
-
 const router = express.Router();
+const {
+  getUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require("../controller/userController");
 
-// Define routes
-router.get("/", getUsers);
-router.post("/", createUser);
+const { protect, admin } = require("../middleware/authMiddleware");
 
-module.exports = router;
+// --- GROUP 1: Root Routes ("/") ---
+// 1. Get all users (Admin only)
+// 2. Create a user manually (Admin only - usually done via /register)
+
+router.route("/")
+    .get(protect, admin, getUsers)
+    .post(protect, admin, createUser);
+
+// --- GROUP 2: ID Routes ("/:id") ---
+// 1. Get One User
+// 2. Update User
+// 3. Delete User (Admin only)
+router.route("/:id")
+    .get(protect,getUserById)   // Controller must check: Is it ME or am I Admin?
+    .put(protect, updateUser)   // Controller must check: Is it ME or am I Admin?
+    .delete(protect, admin, deleteUser);
+    
+module.exports = router;    
